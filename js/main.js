065fff344fba7a809ab41d7e2a6bcc9ee44eff72
DenisovDenisov <denis.demchuk.96@mail.ru>
2015-08-05 16:00:56 +0300

    Добавление магазина

--- js/main.js
+++ js/main.js
@@ -1,29 +1,72 @@
+function Shops () {
+  this._shops = {};
+  this._products = {};
+};
+
+Shops.prototype.setId = (function() {
+    var a = 1;
+    return function () {
+        return a++;
+    };
+})();
+
+Shops.prototype.create = function(data){
+  data.id = this.setId();
+  return this._shops[data.id] = data;
+};
+
+Shops.prototype.addProduct = function(data){
+  data.id = this.setId();
+  return this._products[data.id] = data;
+};
+
+
+var Shop = new Shops();
+
+
+
+function Product () {
+
+};
+
+function Client () {
+
+};
+
+
+
 var shop1 = Shop.create({name : 'shop1' , city : 'Kazan'});
-var shop1 = Shop.create({name: "Shop1", city: "Kazan"});
-var shop2 = Shop.create({name: "Shop2", city: "Moscow"});
-var shop3 = Shop.create({name: "Shop3", city: "Kazan"});
-var shop4 = Shop.create({name: "Shop4", city: "Moscow"});
+
+// var shop2 = Shop.create({name: 'Shop2', city: 'Moscow'});
+// var shop3 = Shop.create({name: "Shop3", city: "Kazan"});
+// var shop4 = Shop.create({name: "Shop4", city: "Moscow"});
 
 
 shop1.addProduct({name: "T­Shirt", price: 100, amount: 50});
-shop1.addProduct({name: "Cool T­Shirt", price: 200, amount: 50});
-shop2.addProduct({name: "T­Shirt", price: 150, amount: 10});
-shop3.addProduct({name: "iPhone 6", price: 50000, amount: 50});
+// shop1.addProduct({name: "Cool T­Shirt", price: 200, amount: 50});
+// shop2.addProduct({name: "T­Shirt", price: 150, amount: 10});
+// shop3.addProduct({name: "iPhone 6", price: 50000, amount: 50});
 
 
-var shops = Shop.getAll();
-var shops_in_kazan = Shop.findByCity("Kazan");
-var products = Product.getAll();
-var tshirts = Product.find("name", "t­shirt");
+// var shops = Shop.getAll();
+// var shops_in_kazan = Shop.findByCity("Kazan");
+// var products = Product.getAll();
+// var tshirts = Product.find("name", "t­shirt");
 
 
-shop3.name = "The Best Shop";
-shop3 = Shop.update(shop3);
-client = Client.create({name: "John Smith", city: "Kazan"});
-client.refund(1000); // пополнить денежный счет
+// shop3.name = "The Best Shop";
+// shop3 = Shop.update(shop3);
+// client = Client.create({name: "John Smith", city: "Kazan"});
+// client.refund(1000); // пополнить денежный счет
 
-var product = tshirts[0];
-client.addToOrder(product);
-client.purchase();
-client.addToOrder(tshirts[0]);
-client.getOrders();
+// var product = tshirts[0];
+// client.addToOrder(product);
+// client.purchase();
+// client.addToOrder(tshirts[0]);
+// client.getOrders();
+
+
+console.log(shop1);
+
+
+
